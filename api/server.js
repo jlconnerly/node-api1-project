97f5acbf3b9e02d00caf723d6a662298a5e709f5
Jake <jlconnerly@gmail.com>
2022-01-06 15:43:39 -0600

    POST a user and GET all users complete

--- api/server.js
+++ api/server.js
@@ -1,9 +1,43 @@
 // IMPORTS
 const express = require('express')
-const user = require('./users/model')
+const res = require('express/lib/response')
+const Users = require('./users/model')
 
 // EXPRESS INSTANCE
 const server = express()
 server.use(express.json())
+
+// [ POST ] a User
+server.post('/api/users', async (request, response) => {
+    try {
+        if (!request.body.name || !request.body.bio) {
+            response.status(400).json({
+               message: "name and bio required" 
+            })
+        } else {
+            const newUser = await Users.insert(request.body)
+            response.status(201).json(newUser)
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Something bad happend',
+            error: err.message,
+        })
+    }
+})
+
+// [ GET ] All Users
+server.get('/api/users', async (request, response) => {
+    try {
+        const users = await Users.find()
+        response.json(users)
+    } catch (error) {
+        response.status(500).json({
+            message: "Unable to fetch all Users",
+            error: error.message
+        })
+    }
+})
+
 
 module.exports = server 
